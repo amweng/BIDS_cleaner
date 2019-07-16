3f049586335676828a15fcc4536e03e911c34e9c
awengca <andrewweng@wustl.edu>
2019-07-16 14:20:30 -0500

    throw error if pathname not set

--- BIDS_tool.m
+++ BIDS_tool.m
@@ -4,15 +4,43 @@ function BIDS_tool()
 % tool to auto-correct errors in BIDS data. (structure/syntax/convention)
 %----------------------------------------------------------------------------------
 
-    % pointme at the top-level bids directory
-    directory = '/Users/andrewweng/Data/ds000107';
+    % pointme at the top-level BIDS directory
+    % 
+    % Important: remember to add path to data
+    %---------------------------------------------
+  %  directory = '/Users/andrewweng/Data/ds000107';
+    directory = '/Users/andrewweng/Data/ds000116';
+    %---------------------------------------------
+    
+    %Taskqueue
     listContents(directory);
     jsonFiles = (trackJson(directory));
     fixJson(jsonFiles,directory);
+    
+    %complete
     disp('BIDS repair complete')
+    disp('done');
 end
 
 
+
+
+
+
+
+
+
+
+%------------------------------------------------------------------------
+% Functions -------------------------------------------------------------
+%------------------------------------------------------------------------
+%   functions: listContents(rootDirectory)
+%              jsonFiles(rootDirectory)
+%              fixJson(jsonFiles,rootDirectory)
+%              spaceToUnderscore(dirtyStr)
+%
+%
+%
 %------------------------------------------------------------------------
 % returns all non-dotfiles in the data directory
 %------------------------------------------------------------------------
@@ -44,26 +72,25 @@ end
 %------------------------------------------------------------------------
 
 function fixJson(jsonFiles,dataDirectory)
-
-    
-    for i = 1:numel(jsonFiles)
-        
-        %displays contents of json files
+    for i = 1:numel(jsonFiles)    
         disp(" ");
         disp("file being repaired: " + jsonFiles(i).name);
-        disp("_________________________________________________________");
         currentFilename = jsonFiles(i).name;
         currentFile = fopen(currentFilename,'r+');
+        
+        if (currentFile == -1)
+            msg = ("Error opening: " + currentFilename);
+            error(msg);
+            break
+        end 
+       
         raw = fread(currentFile);
         chars = char(raw);
         dirtyStr = convertCharsToStrings(chars);
-        %disp(dirtyStr);
         fclose(currentFile);
         currentFilenameStr = string(currentFilename);
     
-        if(currentFilenameStr ~= 'dataset_description.json')
-            
-         
+        if(currentFilenameStr ~= 'dataset_description.json')     
             stepZeroStr = spaceToUnderscore(dirtyStr);
             dirtyJsonStr = jsonencode(stepZeroStr);   
             stepOneStr = strrep(dirtyJsonStr,'\n','');
@@ -71,45 +98,41 @@ function fixJson(jsonFiles,dataDirectory)
             stepThreeStr = strrep(stepTwoStr,'\','');
             stepFourStr = strrep(stepThreeStr,'"{','{');
             cleanStr = strrep(stepFourStr,'}"','}');
-    
+   
             filename = fullfile(dataDirectory, currentFilenameStr);
             fid = fopen(filename, 'w');
             if fid == -1, error('Cannot create JSON file'); end
             fwrite(fid, cleanStr, 'char');
             fclose(fid);
-            
-            disp("repair complete on: " + currentFilenameStr + " ...");
-     
-            
-            %TODO save json file back to where it came from
-         %   fprintf(currentFile,'%s',cleanedStr);
-            
+            disp("repair COMPLETE on: " + currentFilenameStr + " ...");
+
         else
-            cleanedStr = dirtyStr;
             disp("no repair required on: dataset_description.json ...");
         end
         disp(" ");
-        
-        
-        
-  
-    
     end
-    
-        
-        
-        
+     disp("------------------------------------------------------");
+     disp("JSON repair on root volume COMPLETE");
+     disp("------------------------------------------------------");
 end
 
-
-
-%------------------------------------------------------------------------
-% converts spaces in strings to underscores and returns the cleaned string
-%------------------------------------------------------------------------
+%-------------------------------------------------------------------------
+% convert spaces in strings to underscores and returns the cleaned string
+%-------------------------------------------------------------------------
     
 function cleanedStr = spaceToUnderscore(dirtyStr)
     cleanedStr = strrep(dirtyStr," ",'');
 end
+
+
+%-------------------------------------------------------------------------
+% checks for "n/a" onset times and deletes rows where present
+%-------------------------------------------------------------------------
+
+function fixOneSubjectTSV(subjectPath)
+
+end
+
 
 
     
